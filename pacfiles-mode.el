be986780b11770c32ad5c41a6f4a5e8759bd8554
UndeadKernel <carloscg@gmail.com>
2018-07-10 15:53:47 +0200

    Create one single frame and restore old win config

---
+++ pacfiles-mode.el
@@ -0,0 +1,38 @@
+;;; pacfiles-mode.el --- Definition of the pacfiles Major mode -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;; Code:
+
+(require 'pacfiles-win)
+
+(defun pacfiles ()
+  "Find and manage pacman backup files in an Arch-based GNU/Linux system."
+  (interactive)
+  ;; Save the current window configuration so that it can be restored when we are finished.
+  (pacfiles//save-window-conf)
+  (let ((buffer (get-buffer-create pacfiles--files-buffer-name)))
+    (display-buffer buffer '(pacfiles//display-buffer-fullscreen))
+    (with-current-buffer buffer
+      (pacfiles-mode))))
+
+(define-derived-mode pacfiles-mode special-mode "pacfiles"
+  :syntax-table nil
+  :abbrev-table nil
+  "Major mode for managing .pacnew and. pacsave files."
+  (buffer-disable-undo)
+  (setq show-trailing-whitespace nil)
+  ;; Disable lines numbers.
+  (when (bound-and-true-p global-linum-mode)
+    (linum-mode -1))
+  (when (and (fboundp 'nlinum-mode)
+             (bound-and-true-p global-nlinum-mode))
+    (nlinum-mode -1))
+  (when (and (fboundp 'display-line-numbers-mode)
+             (bound-and-true-p global-display-line-numbers-mode))
+    (display-line-numbers-mode -1))
+  ;; Set our key-bindings
+  (define-key pacfiles-mode-map (kbd "q") #'pacfiles/quit))
+
+
+(provide 'pacfiles-mode)
+;;; pacfiles.el ends here
--- pacfiles-win.el
+++ pacfiles-win.el
@@ -3,11 +3,21 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'subr-x)
+
 (defvar pacfiles--files-buffer-name "*pacfiles-file-list*"
   "Name of the window that holds the list of pacman files.")
 
 (defvar pacfiles--previous-window-conf nil
-  "The window configuration before having started `pacfiles'.")
+  "The window configuration before `pacfiles' is called.")
+
+(defun pacfiles/quit ()
+  "Quit pacfiles-mode and restore the previous window configuration."
+  (interactive)
+  (let ((buffer (get-buffer pacfiles--files-buffer-name)))
+    (pacfiles//restore-window-conf)
+    (when buffer
+      (kill-buffer buffer))))
 
 (defun pacfiles//display-buffer-fullscreen (buffer alist)
   "Display BUFFER fullscreen taking ALIST into account."
@@ -19,21 +29,18 @@
     window))
 
 (defun pacfiles//save-window-conf ()
-  "Save the current window configuration to later be restored by `pacfiles--restore-windows-conf'."
-  (unless (get-buffer-window pacfiles--files-buffer-name (selected-frame))
+  "Save the current window configuration to later be restored by `pacfiles//restore-window-conf'."
+  (unless pacfiles--previous-window-conf
     (setq pacfiles--previous-window-conf (current-window-configuration))))
 
-(defun pacfiles//restore-window-conf (&optional kill-buffer)
+(defun pacfiles//restore-window-conf ()
   "Bury or kill pacfiles' buffers according to KILL-BUFFER and restore the previous window configuration."
-  (let ((winconf pacfiles--previous-window-conf)
-        (buffer (current-buffer))
-        (frame (selected-frame)))
-    (quit-window kill-buffer (selected-window))
-    (when (and winconf (equal frame (window-configuration-frame winconf)))
-      (set-window-configuration winconf)
-      (when (buffer-live-p buffer)
-        (with-current-buffer buffer
-          (setq pacfiles--previous-window-conf nil))))))
+  (when pacfiles--previous-window-conf
+    (condition-case nil
+        (progn
+          (set-window-configuration pacfiles--previous-window-conf)
+          (setq pacfiles--previous-window-conf nil))
+      (error (error "Window configuration could not be restored")))))
 
 (provide 'pacfiles-win)
 ;;; pacfiles-win.el ends here
